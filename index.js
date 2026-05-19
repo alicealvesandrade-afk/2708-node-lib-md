015d3cd2ed8ea7a97054121e30663114a07fc506
alicealvesandrade-afk <alice.alves.andrade@escola.pr.gov.br>
2026-05-19 15:07:12 +0000

    Trabalho 2 Finalizado

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
+import fs from 'fs';
 import chalk from 'chalk';
-console.log(chalk.magenta('Cultive o que te faz bem'));
+
+function trataErro(erro) {
+    console.log(erro);
+    throw new Error(chalk.red(erro.code, 'não há arquivo no diretório'));
+}
+
+async function pegaArquivo(caminhoDoArquivo) {
+    try {
+        const encoding = 'utf-8';
+        const texto = await fs.promises.readFile(caminhoDoArquivo, encoding)
+        console.log(chalk.green(texto));
+    } catch (erro) {
+        trataErro(erro)
+    }
+}
+
+pegaArquivo('./arquivos/texto.md');
+pegaArquivo('./arquivos/');
