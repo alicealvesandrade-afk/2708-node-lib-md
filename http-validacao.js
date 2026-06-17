350a9d4d19a0025a2d3de4b61a7830118a94391c
alicealvesandrade-afk <alice.alves.andrade@escola.pr.gov.br>
2026-06-17 11:59:23 +0000

    Trabalho 5 finalizado

---
+++ http-validacao.js
@@ -0,0 +1,29 @@
+import chalk from "chalk";
+
+function extraiLinks (arrLinks) {
+    return arrLinks.map((objetoLink) => Object.values(objetoLink).join())
+}
+
+async function checaStatus (listaURLs) {
+    const arrStatus = await Promise
+    .all(
+      listaURLs.map(async (url) => {
+        try {
+          const response = await fetch(url)
+          return response.status;
+        } catch (erro) {
+          return manejaErros(erro);
+        }
+      })
+
+    )
+    return arrStatus;
+}
+
+function manejaErros (erro) {
+    if (erro.cause.code === 'ENOTFOUND') {
+      return 'link não encontrado';  
+    } else {
+      return 'ocorreu algum erro'  
+    }
+}
